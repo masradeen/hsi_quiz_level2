6a950f78428578bec13cda13f1444af3eb0230d2
MasRadeen <sigitnugrohop@gmail.com>
2024-02-13 15:35:12 +0800

    Menambahkan body: bagian pertama

--- lib/pages/beranda_pages.dart
+++ lib/pages/beranda_pages.dart
@@ -7,42 +7,6 @@ class BerandaPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      // appBar: AppBar(
-      //   backgroundColor: Color(0xFF043164),
-      //   leading: Container(
-      //     color: Colors.amber,
-      //     padding: EdgeInsets.all(8),
-      //     child: Image.asset(
-      //       "assets/logos/hsi.png",
-      //     ),
-      //   ),
-      //   title: Container(
-      //     margin: EdgeInsets.only(
-      //       left: 0
-      //     ),
-          
-      //     color: Colors.amber,
-      //     child: Row(
-      //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      //       children: [
-      //         Text(
-      //           "EDU HSI",
-      //           style: TextStyle(
-      //             fontWeight: FontWeight.bold,
-      //             color: Colors.white,
-      //           ),
-      //         ),
-      //         Text(
-      //           "v.2401-2001",
-      //           style: TextStyle(
-      //             fontSize: 13,
-      //             color: Colors.white,
-      //           ),
-      //         ),
-      //       ],
-      //     ),
-      //   ),
-      // ),
       appBar: AppBar(
         backgroundColor: Color(0xFF043164),
         leading: Stack(
@@ -77,6 +41,29 @@ class BerandaPage extends StatelessWidget {
             ],
           ),
         ),
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 13),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 10,),
+                Text("Assalamu'alaykum,"),
+                Text(
+                  "SIGIT NUGROHO PUTRA",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 5,),
+                Text("ARN241-40181"),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
